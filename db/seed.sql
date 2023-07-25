8c4f2760f6ab24f40d24118c30cd6eed9c5356e2
PatDMcG <110838464+PatDMcG@users.noreply.github.com>
2023-07-25 15:30:41 -0400

    Update seed.sql

--- db/seed.sql
+++ db/seed.sql
@@ -19,6 +19,7 @@ CREATE TABLE game_info (
     systems_requirements TEXT,
     more_like_this TEXT ARRAY,
     game_img TEXT ARRAY
+    price VARCHAR(10)
 );
 
 CREATE TABLE tags (
@@ -48,10 +49,14 @@ INSERT INTO game_info (
     about_this_game,
     systems_requirements,
     more_like_this,
-    game_img
+    game_img,
+    price
 )
 VALUES
-    ('Spooder','horror', '13-Jan-08', 'Valve', 'Gamestop', 'Creole', 'I mean...its alright', 'gtx 4080', '{unique}', '{http://4.bp.blogspot.com/-QuduMdSGyA0/UtUcqratYXI/AAAAAAAABfA/0MYqrVP0qv4/s1600/web-logo1.jpg}');
+    ('Spider','Indie Games', 'Jun 13,2023', 'undownsidable', 'undownsidable', 'English,Simplified Chinese', `Spider is a unique puzzle game that puts players in the role of a brave spider, exploring a variety of stunning environments and overcoming challenges. Players need to use the spider's special abilities and intelligence to solve puzzles and complete the objectives of each level.
+Game Features:
+Rich and diverse levels: There will be multiple levels in the game, each with unique terrain, traps and puzzles, providing players with challenges and fun.
+Spider Abilities: Players can use the spider's special abilities such as climbing walls, spinning silk, and jumping to explore the level`, '{MINIMUM: {OS: `windows`, Storage: `881 MB available space`}, RECOMMENDED: {Storage: `881 MB available space`}}', '{unique}', '{http://4.bp.blogspot.com/-QuduMdSGyA0/UtUcqratYXI/AAAAAAAABfA/0MYqrVP0qv4/s1600/web-logo1.jpg}', '1.99');
 
 INSERT INTO tags (tag_title, tag_link, game_id)
 VALUES 
