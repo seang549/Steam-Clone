b0c91a0ee4f32f8512aa1db60860a7df6f955322
Dorion Boesch <dorion.boesch@gmail.com>
2023-07-25 17:04:19 -0500

    wip for overall CSS outline

---
+++ src/components/GraphForReviews.jsx
@@ -0,0 +1,5 @@
+const GraphForReviews = () => {
+  return <div>Graph For Reviews</div>;
+};
+
+export default GraphForReviews;
---
+++ src/components/Reviews.jsx
@@ -0,0 +1,88 @@
+const Reviews = () => {
+  return (
+    <>
+      <div>Reviews</div>
+      <div>Reviews</div>
+
+      <div>Reviews</div>
+
+      <div>Reviews</div>
+
+      <div>Reviews</div>
+
+      <div>Reviews</div>
+
+      <div>Reviews</div>
+      <div>Reviews</div>
+      <div>Reviews</div>
+      <div>Reviews</div>
+      <div>Reviews</div>
+      <div>Reviews</div>
+      <div>Reviews</div>
+      <div>Reviews</div>
+      <div>Reviews</div>
+      <div>Reviews</div>
+      <div>Reviews</div>
+      <div>Reviews</div>
+      <div>Reviews</div>
+      <div>Reviews</div>
+      <div>Reviews</div>
+      <div>Reviews</div>
+      <div>Reviews</div>
+      <div>Reviews</div>
+      <div>Reviews</div>
+      <div>Reviews</div>
+      <div>Reviews</div>
+      <div>Reviews</div>
+      <div>Reviews</div>
+      <div>Reviews</div>
+      <div>Reviews</div>
+      <div>Reviews</div>
+      <div>Reviews</div>
+      <div>Reviews</div>
+      <div>Reviews</div>
+      <div>Reviews</div>
+      <div>Reviews</div>
+      <div>Reviews</div>
+      <div>Reviews</div>
+      <div>Reviews</div>
+      <div>Reviews</div>
+      <div>Reviews</div>
+      <div>Reviews</div>
+      <div>Reviews</div>
+      <div>Reviews</div>
+      <div>Reviews</div>
+      <div>Reviews</div>
+      <div>Reviews</div>
+      <div>Reviews</div>
+      <div>Reviews</div>
+      <div>Reviews</div>
+      <div>Reviews</div>
+      <div>Reviews</div>
+      <div>Reviews</div>
+      <div>Reviews</div>
+      <div>Reviews</div>
+      <div>Reviews</div>
+      <div>Reviews</div>
+      <div>Reviews</div>
+      <div>Reviews</div>
+      <div>Reviews</div>
+      <div>Reviews</div>
+
+      <div>Reviews</div>
+      <div>Reviews</div>
+      <div>Reviews</div>
+      <div>Reviews</div>
+      <div>Reviews</div>
+      <div>Reviews</div>
+      <div>Reviews</div>
+      <div>Reviews</div>
+      <div>Reviews</div>
+      <div>Reviews</div>
+      <div>Reviews</div>
+      <div>Reviews</div>
+    </>
+  );
+};
+
+export default Reviews;
