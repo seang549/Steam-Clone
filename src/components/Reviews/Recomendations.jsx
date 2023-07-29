b6347712c0b2fd7cb651b372243b82a4b38be3d0
yasminaguilarx <yaguilar2001@gmail.com>
2023-07-29 14:21:14 -0500

    review card css changes

--- src/components/Reviews/Recomendations.jsx
+++ src/components/Reviews/Recomendations.jsx
@@ -1,13 +1,13 @@
 //import { useEffect, useState } from 'react'
-const Recomendations = ({review}) => {
-    let reviewTimeView = true
-    if(review["at_review_time"] == undefined || review["at_review_time"] == null)
-    {
-       reviewTimeView = false
-    }
-    if(review["recommendation"])
-    {
-    
+const Recomendations = ({ review }) => {
+  let reviewTimeView = true;
+  if (
+    review["at_review_time"] == undefined ||
+    review["at_review_time"] == null
+  ) {
+    reviewTimeView = false;
+  }
+  if (review["recommendation"]) {
     return (
       <>
         <div className='SideBySide'>
@@ -23,47 +23,44 @@ const Recomendations = ({review}) => {
               <h2>Recommended</h2>
             </div>
             <div>
-        <h3>{review["total_time"]} hrs on record</h3>
-        {reviewTimeView &&<h3>{` (${review["at_review_time"]} at review time)`}</h3>}
-
+              <h3>{review["total_time"]} hrs on record</h3>
+              {reviewTimeView && (
+                <h3>{` (${review["at_review_time"]} at review time)`}</h3>
+              )}
             </div>
           </div>
         </div>
       </>
     );
-
-    }
-    {
-        return (
-        <>
-        <div className="SideBySide">
+  }
+  return (
+    <>
+      <div className='SideBySide' />
+      <div>
+        <img src='https://store.cloudflare.steamstatic.com/public/shared/images/userreviews/icon_thumbsDown_v6.png'></img>
+      </div>
+      <div className='TopDown'>
         <div>
-        <img src="https://store.cloudflare.steamstatic.com/public/shared/images/userreviews/icon_thumbsDown_v6.png"></img>
+          <h2>Not Recommended</h2>
         </div>
-        <div className="TopDown">
-            <div>
-        <h2>Not Recommended</h2>
-            </div>
-            <div>
-            <h3>{review["total_time"]} hrs on record</h3>
-            {reviewTimeView && <h3>{` (${review["at_review_time"]} at review time)`}</h3>}
-            </div>
+        <div>
+          <h3>{review["total_time"]} hrs on record</h3>
+          {reviewTimeView && (
+            <h3>{` (${review["at_review_time"]} at review time)`}</h3>
+          )}
         </div>
-        <div className='TopDown'>
-          <div>
-            <h2>Not Recommended</h2>
-          </div>
-          <div>
-            <h3>{review["total_time"]}</h3>
-            <h3>{review["at_review_time"]}</h3>
-          </div>
+      </div>
+      <div className='TopDown'>
+        <div>
+          <h2>Not Recommended</h2>
         </div>
+        <div>
+          <h3>{review["total_time"]}</h3>
+          <h3>{review["at_review_time"]}</h3>
+        </div>
+      </div>
+    </>
+  );
+};
 
-        </>
-        )
-    }
-  };
-  
-  export default Recomendations;
-  
-
+export default Recomendations;
--- src/components/Reviews/ReviewCard.jsx
+++ src/components/Reviews/ReviewCard.jsx
@@ -5,11 +5,10 @@ import UserInfo from "./UserInfo";
 import Helpfull from "./Helpfull";
 
 //import { useEffect, useState } from 'react'
-const ReviewCard = ({review}) => {
-if(review !== undefined)
-{
-    let devView = review["developer_response"]
-    console.log(devView)
+const ReviewCard = ({ review }) => {
+  if (review !== undefined) {
+    let devView = review["developer_response"];
+    console.log(devView);
 
     return (
       <>
@@ -18,21 +17,21 @@ if(review !== undefined)
             <div id='user-info-cont'>
               <UserInfo review={review} />
             </div>
-
-        </div>
-        </div>
-        {devView &&
-        <div>
-            <h1>A developer has responded on {review["developer_date"]} @ {review["developer_time"]}</h1>
-
-            <h1>VIEW RESPONSE</h1>
           </div>
         </div>
-        }
+        <div>
+          {devView && (
+            <div>
+              <h1>
+                A developer has responded on {review["developer_date"]} @{" "}
+                {review["developer_time"]}
+              </h1>
+
+              <h1>VIEW RESPONSE</h1>
+            </div>
+          )}
         </div>
-
-        </>
-
+      </>
     );
   }
 };
