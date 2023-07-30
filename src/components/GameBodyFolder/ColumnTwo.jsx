12710aa2948851f9f5b3ff4912d16013f7bda250
Dorion Boesch <dorion.boesch@gmail.com>
2023-07-30 12:01:44 -0500

    90% to  95% of css done for game body

--- src/components/GameBodyFolder/ColumnTwo.jsx
+++ src/components/GameBodyFolder/ColumnTwo.jsx
@@ -1,72 +1,127 @@
 const ColumnTwo = () => {
   return (
-    <div className="flexGap" style={{ flexDirection: "column", gap: "1rem" }}>
+    <div className="flexGap" style={{ flexDirection: "column", gap: "8px" }}>
       <div>
-        <div style={{ minWidth: "308px" }}>Is this game relevant to you?</div>
-        <div>
-          Sign in to see reasons why you may or may not like this based on your
-          games, friends, and curators you follow.
+        <div className="relevent_line">
+          <p>Is this game relevant to you?</p>
         </div>
-        <button className="communityHub" style={{ height: "30px" }}>
-          Sign In
-        </button>{" "}
-        or{" "}
-        <button className="communityHub" style={{ height: "30px" }}>
-          Open in Steam
-        </button>
-      </div>
-
-      <div>
-        <div style={{ display: "flex" }}>
+        <div className="sign_in_btn_ctn">
           <div>
-            <img src="https://store.akamai.steamstatic.com/public/images/v6/ico/ico_singlePlayer.png" />
+            <p style={{ lineHeight: "20px" }}>
+              Sign in to see reasons why you may or may not like this based on
+              your games, friends, and curators you follow.
+            </p>
           </div>
-          Single-player
-        </div>
-        <div style={{ display: "flex" }}>
           <div>
-            <img src="https://store.akamai.steamstatic.com/public/images/v6/ico/ico_learning_about_game.png" />
+            <button className="communityHub" style={{ height: "30px" }}>
+              Sign In
+            </button>{" "}
+            or{" "}
+            <button className="communityHub" style={{ height: "30px" }}>
+              Open in Steam
+            </button>
           </div>
-          Steam is learning about this game (?)
         </div>
-      </div>
-      <div>
-        <div>Languages:</div>
-        <div style={{ display: "flex", justifyContent: "flex-end" }}>
-          Interface Full Audio Subtitles
-        </div>
-        <div>English</div>
-        <div>Simplified Chinese</div>
       </div>
 
-      <div style={{ display: "flex", flexDirection: "column", gap: "2px" }}>
-        <div className="flexGap">
-          TITLE: <div>Spider</div>
+      <div className="cat_ctn clm_two_gradient">
+        <div className="meta_ctn">
+          <div className="cat_icon_ctn">
+            <img
+              className="cat_icon"
+              src="https://store.akamai.steamstatic.com/public/images/v6/ico/ico_singlePlayer.png"
+            />
+          </div>
+          <div className="meta_txt">Single-player</div>
         </div>
-        <div className="flexGap">
-          GENRE: <div>Casual, Indie, Strategy</div>
+        <div className="meta_ctn">
+          <div className="cat_icon_ctn">
+            <img
+              className="cat_icon"
+              src="https://store.akamai.steamstatic.com/public/images/v6/ico/ico_learning_about_game.png"
+            />
+          </div>
+          <div className="learning" style={{ display: "flex", gap: ".5rem" }}>
+            Steam is learning about this game{" "}
+            <div className="question_mark">
+              <span className="qm_txt">
+                This game is not currently eligible to appear in certain
+                showcases on your Steam Profile, and does not contribute to
+                global Achievement or game collector counts.
+              </span>
+            </div>
+          </div>
         </div>
-        <div className="flexGap">
-          DEVELOPER: <div>undownsidable</div>
-        </div>
-        <div className="flexGap">
-          PUBLISHER: <div>undownsidable</div>
-        </div>
-        <div className="flexGap">
-          RELEASE DATE: <div>Jun 13, 2023</div>
-        </div>
-        <div>View update history</div>
-        <div>Read related news</div>
-        <div>View discussions</div>
-        <div>Find Community Groups</div>
       </div>
 
-      <div className="flex" style={{ gap: ".5rem" }}>
-        <div>Share</div>
-        <div>Embed</div>
-        <div>
-          <img className="flag" />
+      <div className="lang_ctn clm_two_gradient">
+        {" "}
+        Languages
+        <table className="lang_table">
+          <tr className="lang_titles lang_row">
+            <th></th>
+            <th>Interface</th>
+            <th>Full Audio</th>
+            <th>Subtitles</th>
+          </tr>
+          <tr className="lang_row">
+            <td className="langs">English</td>
+            <td style={{ color: "#67c1f5" }}>✔</td>
+            <td></td>
+            <td></td>
+          </tr>
+          <tr className="lang_row">
+            <td className="langs">Simplified Chinese</td>
+            <td style={{ color: "#67c1f5" }}>✔</td>
+            <td></td>
+            <td></td>
+          </tr>
+        </table>
+      </div>
+
+      <div
+        className="extra_info_ctn clm_two_gradient"
+        style={{ display: "flex", flexDirection: "column", gap: "2px" }}
+      >
+        <div className="game_info_ctn">
+          <b>TITLE:</b> Spider
+          <br />
+          <b>GENRE:</b>{" "}
+          <span>
+            <a href="">Casual</a>, <a href="">Indie</a>, <a href="">Strategy</a>
+          </span>
+          <br />
+          <b>DEVELOPER:</b> <a href="">undownsidable</a>
+          <br />
+          <b>PUBLISHER:</b> <a href="">undownsidable</a>
+          <br />
+          <b>RELEASE DATE:</b> Jun 13, 2023
         </div>
+        <div className="meta_txt" style={{ height: "25px" }}>
+          View update history
+        </div>
+        <div className="meta_txt" style={{ height: "25px" }}>
+          Read related news
+        </div>
+        <div className="meta_txt" style={{ height: "25px" }}>
+          View discussions
+        </div>
+        <div className="meta_txt" style={{ height: "25px" }}>
+          Find Community Groups
+        </div>
+      </div>
+
+      <div
+        className="flex clm_two_gradient share_flag_ctn"
+        style={{ gap: ".5rem" }}
+      >
+        <div className="communityHub">Share</div>
+        <div className="communityHub">Embed</div>
+        <a href="" className="communityHub">
+          <span>
+            <i className="flag"></i>
+          </span>
+        </a>
       </div>
     </div>
   );
