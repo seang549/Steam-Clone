be66b904090748b3fd370fb4f7b25b527fc71f7b
Dorion Boesch <dorion.boesch@gmail.com>
2023-07-31 21:16:06 -0500

    some css done to the graph and review filter bar

--- src/components/ReviewGraph/ReviewFilters/ReviewDateRange.jsx
+++ src/components/ReviewGraph/ReviewFilters/ReviewDateRange.jsx
@@ -1,60 +1,68 @@
-import React from 'react';
+import React from "react";
 
 const ReviewDateRange = ({ filters, setFilters }) => {
-    const handleDateRangeChange = (event) => {
-        const selectedDateRange = event.target.value;
-        setFilters((prevFilters) => ({
-            ...prevFilters,
-            reviewDateRange: selectedDateRange,
-        }));
-    };
+  const handleDateRangeChange = (event) => {
+    const selectedDateRange = event.target.value;
+    setFilters((prevFilters) => ({
+      ...prevFilters,
+      reviewDateRange: selectedDateRange,
+    }));
+  };
 
-    return (
-        <div className='reviewsFilterMenu'>
-            <div className="title">Date Range</div>
-            <div className='reviewMenuFlyoutContent'>
-                <div className="user_reviews_date_range_explanation">
-                    To view reviews within a date range, please click and drag a selection on a graph above or click on a specific bar.							<br /><br />
-                    <span className="reviewFilterBtn">
-                        <span>Show graph</span>
-                    </span>
-                </div>
-
-                <input
-                    type="radio"
-                    name="review_date_range"
-                    id="review_date_range_all"
-                    value="all"
-                    checked={filters.reviewDateRange === 'all'}
-                    onChange={handleDateRangeChange}
-                />
-                <label htmlFor="review_date_range_all">Lifetime</label><br />
-
-                <input
-                    type="radio"
-                    name="review_date_range"
-                    id="review_date_range_histogram"
-                    value="include"
-                    checked={filters.reviewDateRange === 'include'}
-                    disabled="disabled"
-                    onChange={handleDateRangeChange}
-                />
-                <label htmlFor="review_date_range_histogram">Only Specific Range (Select on graph above)&nbsp;</label><br />
-
-                <input
-                    type="radio"
-                    name="review_date_range"
-                    id="review_date_range_exclude_histogram"
-                    value="exclude"
-                    checked={filters.reviewDateRange === 'exclude'}
-                    disabled="disabled"
-                    onChange={handleDateRangeChange}
-                />
-                <label htmlFor="review_date_range_exclude_histogram">Exclude Specific Range (Select on graph above)&nbsp;</label><br />
-
-            </div>
+  return (
+    <div className="reviewsFilterMenu filter_dropdown">
+      <div className="title">Date Range</div>
+      <div className="reviewMenuFlyoutContent filter_dropdown_content">
+        <div className="user_reviews_date_range_explanation">
+          To view reviews within a date range, please click and drag a selection
+          on a graph above or click on a specific bar. <br />
+          <br />
+          <span className="reviewFilterBtn">
+            <span>Show graph</span>
+          </span>
         </div>
-    );
+
+        <input
+          type="radio"
+          name="review_date_range"
+          id="review_date_range_all"
+          value="all"
+          checked={filters.reviewDateRange === "all"}
+          onChange={handleDateRangeChange}
+        />
+        <label htmlFor="review_date_range_all">Lifetime</label>
+        <br />
+
+        <input
+          type="radio"
+          name="review_date_range"
+          id="review_date_range_histogram"
+          value="include"
+          checked={filters.reviewDateRange === "include"}
+          disabled="disabled"
+          onChange={handleDateRangeChange}
+        />
+        <label htmlFor="review_date_range_histogram">
+          Only Specific Range (Select on graph above)&nbsp;
+        </label>
+        <br />
+
+        <input
+          type="radio"
+          name="review_date_range"
+          id="review_date_range_exclude_histogram"
+          value="exclude"
+          checked={filters.reviewDateRange === "exclude"}
+          disabled="disabled"
+          onChange={handleDateRangeChange}
+        />
+        <label htmlFor="review_date_range_exclude_histogram">
+          Exclude Specific Range (Select on graph above)&nbsp;
+        </label>
+        <br />
+      </div>
+    </div>
+  );
 };
 
 export default ReviewDateRange;
--- src/components/ReviewGraph/ReviewFilters/ReviewFilters.jsx
+++ src/components/ReviewGraph/ReviewFilters/ReviewFilters.jsx
@@ -1,45 +1,56 @@
-import ReviewType from './ReviewType.jsx'
-import PurchaseType from './PurchaseType.jsx'
-import ReviewLanguage from './ReviewLanguage.jsx'
-import ReviewDateRange from './ReviewDateRange.jsx'
-import PlayTime from './PlayTime.jsx'
-import ReviewDisplayAs from './ReviewDisplayAs.jsx'
-import ExpandBtns from './ExpandBtns.jsx'
-import {useState} from 'react'
+import ReviewType from "./ReviewType.jsx";
+import PurchaseType from "./PurchaseType.jsx";
+import ReviewLanguage from "./ReviewLanguage.jsx";
+import ReviewDateRange from "./ReviewDateRange.jsx";
+import PlayTime from "./PlayTime.jsx";
+import ReviewDisplayAs from "./ReviewDisplayAs.jsx";
+import ExpandBtns from "./ExpandBtns.jsx";
+import { useState } from "react";
 
 const ReviewFilters = ({ filters, setFilters, expanded, toggleExpanded }) => {
-    const totalReviewCount = ''
-    const posReviewCount= ''
-    const negReviewCount = ''
-    const steamPurchaseCount = ''
-    const otherPurchaseCount = ''
-    const englishReviews = ''
-    const yourLanguageReviewCount = ''
-    
-    
-   
-          return (
-        <>
-    
-    
-    
-                <div id='reviewFilterOptions'>
-                      <ReviewType filters={filters} setFilters={setFilters} totalReviewCount={totalReviewCount} posReviewCount={posReviewCount} negReviewCount={negReviewCount} />
+  const totalReviewCount = "";
+  const posReviewCount = "";
+  const negReviewCount = "";
+  const steamPurchaseCount = "";
+  const otherPurchaseCount = "";
+  const englishReviews = "";
+  const yourLanguageReviewCount = "";
 
-                      <PurchaseType filters={filters} setFilters={setFilters} totalReviewCount={totalReviewCount} steamPurchaseCount={steamPurchaseCount} otherPurchaseCount={otherPurchaseCount} /> 
-                 
-                      <ReviewLanguage filters={filters} setFilters={setFilters} englishReviews={englishReviews} yourLanguageReviewCount={yourLanguageReviewCount} />
-                    
-                      <ReviewDateRange filters={filters} setFilters={setFilters} />
+  return (
+    <>
+      <div id="reviewFilterOptions">
+        <ReviewType
+          filters={filters}
+          setFilters={setFilters}
+          totalReviewCount={totalReviewCount}
+          posReviewCount={posReviewCount}
+          negReviewCount={negReviewCount}
+        />
 
-                      <PlayTime filters={filters} setFilters={setFilters} />
-                    
-                      <ReviewDisplayAs filters={filters} setFilters={setFilters} />
-                      <ExpandBtns expanded={expanded} toggleExpanded={toggleExpanded} />
-                  </div >
+        <PurchaseType
+          filters={filters}
+          setFilters={setFilters}
+          totalReviewCount={totalReviewCount}
+          steamPurchaseCount={steamPurchaseCount}
+          otherPurchaseCount={otherPurchaseCount}
+        />
 
-        </>
-    )
-}
+        <ReviewLanguage
+          filters={filters}
+          setFilters={setFilters}
+          englishReviews={englishReviews}
+          yourLanguageReviewCount={yourLanguageReviewCount}
+        />
 
-export default ReviewFilters
+        <ReviewDateRange filters={filters} setFilters={setFilters} />
+
+        <PlayTime filters={filters} setFilters={setFilters} />
+
+        <ReviewDisplayAs filters={filters} setFilters={setFilters} />
+        <ExpandBtns expanded={expanded} toggleExpanded={toggleExpanded} />
+      </div>
+    </>
+  );
+};
+
+export default ReviewFilters;
