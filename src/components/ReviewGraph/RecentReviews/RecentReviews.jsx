c2b59a244d80ab2869a6bb487f061b11ae5095b6
yasminaguilarx <yaguilar2001@gmail.com>
2023-08-01 09:25:11 -0500

    changed back to working state

--- src/components/ReviewGraph/RecentReviews/RecentReviews.jsx
+++ src/components/ReviewGraph/RecentReviews/RecentReviews.jsx
@@ -1,38 +1,81 @@
 import RecentReviewsChart from "./RecentReviewsChart.jsx";
 
+const AllReviews = ({
+  setMinDate,
+  setMaxDate,
+  recentPermData,
+  setRecentPermData,
+  data,
+}) => {
+  const reviewSumm = "ChangeMe";
+  const tooltipText = "ChangeMe";
+  const reviewNumText = "changeMe";
 
-const AllReviews = ({ setMinDate, setMaxDate, recentPermData, setRecentPermData, data }) => {
-    const reviewSumm = 'ChangeMe'
-    const tooltipText = 'ChangeMe'
-    const reviewNumText = 'changeMe'
-
+  //   return (
+  //     <>
+  //       <div className="user_reviews_summary_bar">
+  //         <div className="summary_section">
+  //           <div className="title">Overall Reviews:</div>
+  //           <span
+  //             id="recent_game_review_summary"
+  //             className={reviewSumm}
+  //             data-tooltip-html={tooltipText}
+  //           >
+  //             {reviewSumm}
+  //           </span>
+  //           <span className="reviewNum">{reviewNumText}</span>
+  //           <a
+  //             className="tooltip"
+  //             data-tooltip-text="This summary uses only reviews writtem by customers that purchase the game directly from Steam."
+  //           >
+  //             <img src="https://store.akamai.steamstatic.com/public/shared/images/ico/icon_questionmark.png"></img>
+  //           </a>
+  //         </div>
+  //         <div
+  //           id="recent_review_histogram_rollup_container"
+  //           className="review_histogram"
+  //         >
+  //           <div className="review_histogram_rollup">
+  //             <RecentReviewsChart data={data} />
+  //           </div>
+  //         </div>
+  //       </div>
+  //     </>
+  //   );
+  // };
 
   return (
     <>
-      <div className="user_reviews_summary_bar">
-        <div className="summary_section">
-          <div className="title">Overall Reviews:</div>
+      <div className='user_reviews_summary_bar'>
+        <div className='summary_section'>
+          <div className='title'>Overall Reviews:</div>
           <span
-            id="recent_game_review_summary"
+            id='recent_game_review_summary'
             className={reviewSumm}
             data-tooltip-html={tooltipText}
           >
             {reviewSumm}
           </span>
-          <span className="reviewNum">{reviewNumText}</span>
+          <span className='reviewNum'>{reviewNumText}</span>
           <a
-            className="tooltip"
-            data-tooltip-text="This summary uses only reviews writtem by customers that purchase the game directly from Steam."
+            className='tooltip'
+            data-tooltip-text='This summary uses only reviews writtem by customers that purchase the game directly from Steam.'
           >
-            <img src="https://store.akamai.steamstatic.com/public/shared/images/ico/icon_questionmark.png"></img>
+            <img src='https://store.akamai.steamstatic.com/public/shared/images/ico/icon_questionmark.png'></img>
           </a>
         </div>
         <div
-          id="recent_review_histogram_rollup_container"
-          className="review_histogram"
+          id='recent_review_histogram_rollup_container'
+          className='review_histogram'
         >
-          <div className="review_histogram_rollup">
-            <RecentReviewsChart data={data} />
+          <div className='review_histogram_rollup'>
+            <RecentReviewsChart
+              setMinDate={setMinDate}
+              setMaxDate={setMaxDate}
+              recentPermData={recentPermData}
+              setRecentPermData={setRecentPermData}
+              data={data}
+            />
           </div>
         </div>
       </div>
@@ -40,27 +83,4 @@ const AllReviews = ({ setMinDate, setMaxDate, recentPermData, setRecentPermData,
   );
 };
 
-
-    return (
-        <>
-            <div className="user_reviews_summary_bar">
-                <div className="summary_section">
-                    <div className="title">Overall Reviews:</div>
-                    <span id="recent_game_review_summary" className={reviewSumm} data-tooltip-html={tooltipText}>{reviewSumm}</span>
-                    <span className='reviewNum'>{reviewNumText}</span>
-                    <a className='tooltip' data-tooltip-text='This summary uses only reviews writtem by customers that purchase the game directly from Steam.'>
-                        <img src="https://store.akamai.steamstatic.com/public/shared/images/ico/icon_questionmark.png"></img>
-                    </a>
-                </div>
-                <div id='recent_review_histogram_rollup_container' className="review_histogram">
-                    <div className="review_histogram_rollup">
-                        <RecentReviewsChart setMinDate={setMinDate} setMaxDate={setMaxDate} recentPermData={recentPermData} setRecentPermData={setRecentPermData} data={data} />
-                    </div>
-                </div>
-            </div>
-        </>
-    )
-}
-
-export default AllReviews
-
+export default AllReviews;
--- src/components/ReviewGraph/ReviewFilters/ReviewFilters.jsx
+++ src/components/ReviewGraph/ReviewFilters/ReviewFilters.jsx
@@ -7,41 +7,62 @@ import ReviewDisplayAs from "./ReviewDisplayAs.jsx";
 import ExpandBtns from "./ExpandBtns.jsx";
 import { useState } from "react";
 
+const ReviewFilters = ({
+  fullData,
+  filters,
+  setFilters,
+  expanded,
+  toggleExpanded,
+}) => {
+  const totalReviewCount = "";
+  const posReviewCount = "";
+  const negReviewCount = "";
+  const steamPurchaseCount = "";
+  const otherPurchaseCount = "";
+  const englishReviews = "";
+  const yourLanguageReviewCount = "";
 
-const ReviewFilters = ({ fullData, filters, setFilters, expanded, toggleExpanded }) => {
-    const totalReviewCount = ''
-    const posReviewCount= ''
-    const negReviewCount = ''
-    const steamPurchaseCount = ''
-    const otherPurchaseCount = ''
-    const englishReviews = ''
-    const yourLanguageReviewCount = ''
-    
-    
-   
-          return (
-        <>
-    
-    
-    
-                <div id='reviewFilterOptions'>
-                      <ReviewType filters={filters} setFilters={setFilters} totalReviewCount={totalReviewCount} posReviewCount={posReviewCount} negReviewCount={negReviewCount} />
+  return (
+    <>
+      <div id='reviewFilterOptions'>
+        <ReviewType
+          filters={filters}
+          setFilters={setFilters}
+          totalReviewCount={totalReviewCount}
+          posReviewCount={posReviewCount}
+          negReviewCount={negReviewCount}
+        />
 
-                      <PurchaseType filters={filters} setFilters={setFilters} totalReviewCount={totalReviewCount} steamPurchaseCount={steamPurchaseCount} otherPurchaseCount={otherPurchaseCount} /> 
-                 
-                      <ReviewLanguage filters={filters} setFilters={setFilters} englishReviews={englishReviews} yourLanguageReviewCount={yourLanguageReviewCount} />
-                    
-                      <ReviewDateRange expanded={expanded} toggleExpanded={toggleExpanded} fullData={fullData} filters={filters} setFilters={setFilters} />
+        <PurchaseType
+          filters={filters}
+          setFilters={setFilters}
+          totalReviewCount={totalReviewCount}
+          steamPurchaseCount={steamPurchaseCount}
+          otherPurchaseCount={otherPurchaseCount}
+        />
 
-                      <PlayTime filters={filters} setFilters={setFilters} />
-                    
-                      <ReviewDisplayAs filters={filters} setFilters={setFilters} />
-                      <ExpandBtns expanded={expanded} toggleExpanded={toggleExpanded} />
-                  </div >
+        <ReviewLanguage
+          filters={filters}
+          setFilters={setFilters}
+          englishReviews={englishReviews}
+          yourLanguageReviewCount={yourLanguageReviewCount}
+        />
 
-        </>
-    )
-}
+        <ReviewDateRange
+          expanded={expanded}
+          toggleExpanded={toggleExpanded}
+          fullData={fullData}
+          filters={filters}
+          setFilters={setFilters}
+        />
 
-export default ReviewFilters
+        <PlayTime filters={filters} setFilters={setFilters} />
 
+        <ReviewDisplayAs filters={filters} setFilters={setFilters} />
+        <ExpandBtns expanded={expanded} toggleExpanded={toggleExpanded} />
+      </div>
+    </>
+  );
+};
+
+export default ReviewFilters;
