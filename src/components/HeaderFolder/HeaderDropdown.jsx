3dafb5d5bd716117d12594a3197258b92ec109ba
PatDMcG <110838464+PatDMcG@users.noreply.github.com>
2023-08-01 17:36:21 -0400

    UPDATED HEADER NAV BAR

--- src/components/HeaderFolder/HeaderDropdown.jsx
+++ src/components/HeaderFolder/HeaderDropdown.jsx
@@ -1,6 +1,7 @@
-import React, { useState } from "react";
 
-const HeaderDropdown = ({ submenus, i }) => {
+import React, { useState } from 'react';
+
+const HeaderDropdown = ({ submenus }) => {
   const [activeSubmenu, setActiveSubmenu] = useState(null);
 
   const handleMouseEnter = (index) => {
@@ -11,64 +12,25 @@ const HeaderDropdown = ({ submenus, i }) => {
     setActiveSubmenu(null);
   };
 
-  if (i === 2) {
-    return (
-      <ul style={{ display: "flex", flex: "column" }} className='categories'>
-        {submenus.map((submenu, index) => (
-          <li
-            key={index}
-            className='dropdown-item'
-            style={{ listStyleType: "none" }}
-            onMouseEnter={() => handleMouseEnter(index)}
-            onMouseLeave={handleMouseLeave}
-          >
-            <div className={`subtitle-div subtitle${index}`}>
-              <a href={submenu.url}>{submenu.subTitle}</a>
-            </div>
-            <div className={`categories-hr${index}`}></div>
-            <div className={`categories-vr${index}`}></div>
-            <div className={`title-div title${index}`}>
-              <a href={submenu.url}>{submenu.title}</a>
-            </div>
-            {activeSubmenu === index &&
-              submenu.dropdownItems &&
-              submenu.dropdownItems.length > 0 && (
-                <ul className='submenu'>
-                  {submenu.dropdownItems.map((item, i) => (
-                    <li key={i}>
-                      <a href={item.url}>{item.title}</a>
-                    </li>
-                  ))}
-                </ul>
-              )}
-          </li>
-        ))}
-      </ul>
-    );
-  }
-
   return (
-    <ul className='dropdown' style={{ display: "flex", flex: "column" }}>
+    <ul style={{listStyleType: "none"}} className='dropdown-header'>
       {submenus.map((submenu, index) => (
         <li
           key={index}
-          className='dropdown-item-store-and-new'
-          style={{ listStyleType: "none" }}
+          className="dropdown-item-header"
           onMouseEnter={() => handleMouseEnter(index)}
           onMouseLeave={handleMouseLeave}
         >
           <a href={submenu.url}>{submenu.title}</a>
-          {activeSubmenu === index &&
-            submenu.dropdownItems &&
-            submenu.dropdownItems.length > 0 && (
-              <ul className='submenu'>
-                {submenu.dropdownItems.map((item, i) => (
-                  <li key={i}>
-                    <a href={item.url}>{item.title}</a>
-                  </li>
-                ))}
-              </ul>
-            )}
+          {activeSubmenu === index && submenu.dropdownItems && submenu.dropdownItems.length > 0 (
+            <ul className="submenu">
+              {submenu.dropdownItems.map((item, i) => (
+                <li key={i}>
+                  <a href={item.url}>{item.title}</a>
+                </li>
+              ))}
+            </ul>
+          )}
         </li>
       ))}
     </ul>
