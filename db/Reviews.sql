ff09ecade32531b61109aa3bb994e229ac769b93
PatDMcG <110838464+PatDMcG@users.noreply.github.com>
2023-08-04 13:52:33 -0400

    partition by date

--- db/Reviews.sql
+++ db/Reviews.sql
@@ -17,3 +17,26 @@ CREATE TABLE Reviews(
     recomendation BOOLEAN,
     body TEXT
 );
+
+CREATE OR REPLACE FUNCTION Reviews_function() RETURNS trigger AS
+$BODY$
+DECLARE
+partition_posted TEXT;
+partition TEXT;
+BEGIN
+partition_posted := to_char(NEW.posted,'YYYY_MM_DD');
+partition := TG_RELNAME || '_' || partition_posted;
+IF NOT EXISTS(SELECT relname FROM pg_class WHERE relname=partition) THEN
+RAISE NOTICE 'A partition has been created %',partition;
+EXECUTE 'CREATE TABLE ' || partition || ' (check (posted = ''' || NEW.posted || ''')) INHERITS (' || TG_RELNAME || ');';
+END IF;
+EXECUTE 'INSERT INTO ' || partition || ' SELECT(' || TG_RELNAME || ' ' || quote_literal(NEW) || ').* RETURNING posted;';
+RETURN NULL;
+END;
+$BODY$
+LANGUAGE plpgsql VOLATILE
+COST 100;
+
+CREATE TRIGGER Reviews_trigger
+BEFORE INSERT ON Parent
+FOR EACH ROW EXECUTE PROCEDURE partition_function();
